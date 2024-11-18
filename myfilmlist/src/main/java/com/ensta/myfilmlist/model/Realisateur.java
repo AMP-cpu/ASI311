a83076f0f336e70983cdc8f746f460a87bdd6c55
melclipse15 <doramelissa.konlackfoning01@universitadipavia.it>
2024-11-18 11:32:12 +0100

    Classe realisateur ajournée

--- myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
+++ myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
@@ -22,5 +22,23 @@ public class Realisateur {
         this.dateNaissance = dateNaissance;
         this.celebre = celebre;
     }
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public LocalDate getDateNaissance() { return dateNaissance; }
+    public void setDateNaissance(LocalDate dateNaissance) { this.dateNaissance = dateNaissance; }
+
+    public List<Film> getFilmRealises() { return filmRealises; }
+    public void setFilmRealises(List<Film> filmRealises) { this.filmRealises = filmRealises; }
+
+    public boolean isCelebre() { return celebre; }
+    public void setCelebre(boolean celebre) { this.celebre = celebre; }
+}
 
 }

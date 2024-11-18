e9de8b4c48227366e3d4d14ee577daf9e74c17ec
a.salima <s.azzouz@oxilog.net>
2024-11-18 11:35:49 +0100

    Classe Realisateur

--- myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
+++ myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
@@ -1,10 +1,8 @@
 package com.ensta.myfilmlist.model;
-
 import java.time.LocalDate;
 import java.util.List;
 
 public class Realisateur {
-
     private long id;
     private String nom;
     private String prenom;
@@ -12,15 +10,23 @@ public class Realisateur {
     private List<Film> filmRealises;
     private boolean celebre;
 
-    public Realisateur() {
-    }
+    // Getters and Setters
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
 
-    public Realisateur(long id, String nom, String prenom, LocalDate dateNaissance, boolean celebre) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.dateNaissance = dateNaissance;
-        this.celebre = celebre;
-    }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
 
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public LocalDate getDateNaissance() { return dateNaissance; }
+    public void setDateNaissance(LocalDate dateNaissance) { this.dateNaissance = dateNaissance; }
+
+    public List<Film> getFilmRealises() { return filmRealises; }
+    public void setFilmRealises(List<Film> filmRealises) { this.filmRealises = filmRealises; }
+
+    public boolean isCelebre() { return celebre; }
+    public void setCelebre(boolean celebre) { this.celebre = celebre; }
 }
+

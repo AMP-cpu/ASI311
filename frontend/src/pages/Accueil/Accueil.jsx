bc012cddce719e9d6963fc93738c9e244c1a9b74
Picolau <lgpicoli@hotmail.com>
2025-02-09 15:33:56 +0100

    add select to director

--- frontend/src/pages/Accueil/Accueil.jsx
+++ frontend/src/pages/Accueil/Accueil.jsx
@@ -4,9 +4,9 @@ import "./Accueil.css";
 import { MovieModal } from "../../components/MovielModal/MovielModal"; // Corrected import
 
 const API_KEY = "17eb4502"; // OMDb API key
-const userId = localStorage.getItem('userId')
-const admin = localStorage.getItem('is_admin')
-console.log(admin)
+const userId = localStorage.getItem("userId");
+const admin = localStorage.getItem("is_admin");
+console.log(admin);
 
 export const Accueil = () => {
   const [movies, setMovies] = useState([]);
@@ -17,8 +17,8 @@ export const Accueil = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [newMovie, setNewMovie] = useState({
     titre: "",
-    duree: 0,
-    realisateurId: 0,
+    duree: undefined,
+    realisateurId: undefined,
   });
   const [newDirector, setNewDirector] = useState({
     prenom: "",
@@ -36,6 +36,10 @@ export const Accueil = () => {
         response.data.forEach((movie) => fetchMovieDetails(movie.titre));
       })
       .catch((error) => console.error("Error fetching movies:", error));
+
+    axios.get("http://localhost:8080/realisateur").then((response) => {
+      setDirectors(response.data);
+    });
 
     axios
       .get(`http://localhost:8080/film/favorite/${userId}`)
@@ -75,8 +79,8 @@ export const Accueil = () => {
     setSelectedMovie({ ...movie, ...movieDetails });
   };
 
-   // Handle new movie form submission
-   const handleNewMovieSubmit = async (e) => {
+  // Handle new movie form submission
+  const handleNewMovieSubmit = async (e) => {
     e.preventDefault();
 
     try {
@@ -100,36 +104,26 @@ export const Accueil = () => {
   };
 
   // Handle new director form submission
-const handleNewDirectorSubmit = async (e) => {
-  e.preventDefault();
+  const handleNewDirectorSubmit = async (e) => {
+    e.preventDefault();
 
-  const newDirectorData = {
-    prenom: "newDirector.prenom",
-    nom: "newDirector.nom",
-    dateNaissance: "newDirector.dateNaissance",
+    try {
+      // Sending the new director data to the server in the required format
+      await axios.post("http://localhost:8080/realisateur", newDirector);
+
+      // Reset the form fields
+      setNewDirector({
+        prenom: "",
+        nom: "",
+        dateNaissance: "",
+      });
+
+      // Hide the form after submission
+      setIsDirectorFormVisible(false);
+    } catch (error) {
+      console.error("Error adding new director:", error);
+    }
   };
-
-  try {
-    // Sending the new director data to the server in the required format
-    const response = await axios.post("http://localhost:8080/realisateur", newDirector);
-    const addedDirector = response.data;
-
-    // Add the new director to the state if necessary, or just clear the form
-    setDirectors([...directors, addedDirector]);
-
-    // Reset the form fields
-    setNewDirector({
-      prenom: "",
-      nom: "",
-      dateNaissance: "",
-    });
-
-    // Hide the form after submission
-    setIsDirectorFormVisible(false);
-  } catch (error) {
-    console.error("Error adding new director:", error);
-  }
-};
 
   return (
     <div className="accueil">
@@ -169,125 +163,167 @@ const handleNewDirectorSubmit = async (e) => {
           <p>No movies found.</p>
         )}
       </div>
-      {favoriteMovies.length>0 && (
+      {favoriteMovies.length > 0 && (
         <>
-        <h2>Favorite Movies</h2>
-              <div className="movie-carousel">
-                {favoriteMovies.length > 0 ? (
-                  favoriteMovies.map((movie) => (
-                    <div
-                      key={movie.id}
-                      className="movie-card"
-                      onClick={() => handleMovieClick(movie)}
-                    >
-                      {omdbData[movie.titre]?.Poster ? (
-                        <img
-                          src={omdbData[movie.titre].Poster}
-                          alt={movie.titre}
-                          className="movie-poster"
-                        />
-                      ) : (
-                        <div className="placeholder">No Image Available</div>
-                      )}
-                      <p>{movie.titre}</p>
-                    </div>
-                  ))
-                ) : (
-                  <p>No favorite movies found.</p>
-                )}
-              </div>
+          <h2>Favorite Movies</h2>
+          <div className="movie-carousel">
+            {favoriteMovies.length > 0 ? (
+              favoriteMovies.map((movie) => (
+                <div
+                  key={movie.id}
+                  className="movie-card"
+                  onClick={() => handleMovieClick(movie)}
+                >
+                  {omdbData[movie.titre]?.Poster ? (
+                    <img
+                      src={omdbData[movie.titre].Poster}
+                      alt={movie.titre}
+                      className="movie-poster"
+                    />
+                  ) : (
+                    <div className="placeholder">No Image Available</div>
+                  )}
+                  <p>{movie.titre}</p>
+                </div>
+              ))
+            ) : (
+              <p>No favorite movies found.</p>
+            )}
+          </div>
         </>
       )}
-      
 
-      <MovieModal selectedMovie={selectedMovie} closeModal={() => setSelectedMovie(null)} />
+      <MovieModal
+        selectedMovie={selectedMovie}
+        closeModal={() => setSelectedMovie(null)}
+      />
 
-      {localStorage.getItem('is_admin')=="true" && (
-      <>
-        {!isFormVisible && (
-          <div className="centered-button">
-            <button className="add-movie-button" onClick={() => setIsFormVisible(true)}>
-              Add New Movie
-            </button>
-          </div>
-        )}
+      {localStorage.getItem("is_admin") == "true" && (
+        <>
+          {!isFormVisible && (
+            <div className="centered-button">
+              <button
+                className="add-movie-button"
+                onClick={() => setIsFormVisible(true)}
+              >
+                Add New Movie
+              </button>
+            </div>
+          )}
 
-        {isFormVisible && (
-          <div className="new-movie-form">
-            <h2>Add a New Movie</h2>
-            <button className="close-button" onClick={() => setIsFormVisible(false)}>
-              Close
-            </button>
-            <form onSubmit={handleNewMovieSubmit}>
-              <input
-                type="text"
-                placeholder="Movie Title"
-                value={newMovie.titre}
-                onChange={(e) => setNewMovie({ ...newMovie, titre: e.target.value })}
-                required
-              />
-              <input
-                type="number"
-                placeholder="Duration (minutes)"
-                value={newMovie.duree}
-                onChange={(e) => setNewMovie({ ...newMovie, duree: parseInt(e.target.value) })}
-                required
-              />
-              <input
-                type="number"
-                placeholder="Director ID"
-                value={newMovie.realisateurId}
-                onChange={(e) => setNewMovie({ ...newMovie, realisateurId: parseInt(e.target.value) })}
-                required
-              />
-              <button type="submit">Add Movie</button>
-            </form>
-          </div>
-        )}
+          {isFormVisible && (
+            <div className="new-movie-form">
+              <h2>Add a New Movie</h2>
+              <button
+                className="close-button"
+                onClick={() => setIsFormVisible(false)}
+              >
+                Close
+              </button>
+              <form onSubmit={handleNewMovieSubmit}>
+                <input
+                  type="text"
+                  placeholder="Movie Title"
+                  value={newMovie.titre}
+                  onChange={(e) =>
+                    setNewMovie({ ...newMovie, titre: e.target.value })
+                  }
+                  required
+                />
+                <input
+                  type="number"
+                  placeholder="Duration (minutes)"
+                  value={newMovie.duree}
+                  onChange={(e) =>
+                    setNewMovie({
+                      ...newMovie,
+                      duree: parseInt(e.target.value),
+                    })
+                  }
+                  required
+                />
+                <select
+                  value={newMovie.realisateurId || ""}
+                  onChange={(e) =>
+                    setNewMovie({
+                      ...newMovie,
+                      realisateurId: parseInt(e.target.value),
+                    })
+                  }
+                  className="director-select"
+                  required
+                >
+                  <option value="" disabled>
+                    Select a Director
+                  </option>
+                  {directors.map((director) => (
+                    <option key={director.id} value={director.id}>
+                      {director.prenom} {director.nom}
+                    </option>
+                  ))}
+                </select>
+                <button type="submit">Add Movie</button>
+              </form>
+            </div>
+          )}
 
-        {!isDirectorFormVisible && (
-          <div className="centered-button">
-            <button className="add-movie-button" onClick={() => setIsDirectorFormVisible(true)}>
-              Add New Director
-            </button>
-          </div>
-        )}
+          {!isDirectorFormVisible && (
+            <div className="centered-button">
+              <button
+                className="add-movie-button"
+                onClick={() => setIsDirectorFormVisible(true)}
+              >
+                Add New Director
+              </button>
+            </div>
+          )}
 
-        {isDirectorFormVisible && (
-          <div className="new-movie-form">
-            <h2>Add a New Director</h2>
-            <button className="close-button" onClick={() => setIsDirectorFormVisible(false)}>
-              Close
-            </button>
-            <form onSubmit={handleNewDirectorSubmit}>
-              <input
-                type="text"
-                placeholder="Director First Name"
-                value={newDirector.prenom}
-                onChange={(e) => setNewDirector({ ...newDirector, prenom: e.target.value })}
-                required
-              />
-              <input
-                type="text"
-                placeholder="Director Last Name"
-                value={newDirector.nom}
-                onChange={(e) => setNewDirector({ ...newDirector, nom: e.target.value })}
-                required
-              />
-              <input
-                type="date"
-                placeholder="Date of Birth"
-                value={newDirector.dateNaissance}
-                onChange={(e) => setNewDirector({ ...newDirector, dateNaissance: e.target.value })}
-                required
-              />
-              <button type="submit">Add Director</button>
-            </form>
-          </div>
-        )}
-      </>
-    )}
-
+          {isDirectorFormVisible && (
+            <div className="new-movie-form">
+              <h2>Add a New Director</h2>
+              <button
+                className="close-button"
+                onClick={() => setIsDirectorFormVisible(false)}
+              >
+                Close
+              </button>
+              <form onSubmit={handleNewDirectorSubmit}>
+                <input
+                  type="text"
+                  placeholder="Director First Name"
+                  value={newDirector.prenom}
+                  onChange={(e) =>
+                    setNewDirector({ ...newDirector, prenom: e.target.value })
+                  }
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Director Last Name"
+                  value={newDirector.nom}
+                  onChange={(e) =>
+                    setNewDirector({ ...newDirector, nom: e.target.value })
+                  }
+                  required
+                />
+                <input
+                  type="date"
+                  placeholder="Date of Birth"
+                  value={newDirector.dateNaissance}
+                  onChange={(e) =>
+                    setNewDirector({
+                      ...newDirector,
+                      dateNaissance: e.target.value,
+                    })
+                  }
+                  required
+                />
+                <button type="submit">Add Director</button>
+              </form>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
